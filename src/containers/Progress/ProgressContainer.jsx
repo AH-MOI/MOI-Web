10be78b19e3a08caa574d4146aa8ec2326850057
정성훈 <52942411+jsh0128@users.noreply.github.com>
2020-11-23 23:39:07 +0900

    [ADD] progress layout add

---
+++ src/containers/Progress/ProgressContainer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { observer } from "mobx-react";
+import Progress from "components/Progress/Progress";
+
+const ProgressContainer = ({}) => {
+  return <Progress />;
+};
+
+export default observer(ProgressContainer);
