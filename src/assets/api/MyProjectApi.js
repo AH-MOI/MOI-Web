f9a34254e8fb633ee8dab9486fa0c4be4ac7962b
정성훈 <52942411+jsh0128@users.noreply.github.com>
2020-11-25 03:40:35 +0900

    {ADD] config.json SERVER_RED, SERVER_BLUE 추가
    modal loading 추가

--- src/assets/api/MyProjectApi.js
+++ src/assets/api/MyProjectApi.js
@@ -1,23 +1,23 @@
-import axios from 'axios';
-import {SERVER} from "../../config/config.json";
+import axios from "axios";
+import { SERVER_BLUE } from "../../config/config.json";
 
 class MyProjectAPI {
-    async GetDevInfo(accessToken) {
-        try {
-            const config = {
-                headers: {
-                    Authorization: accessToken,
-                }
-            }
-            const url = `${SERVER}/info/all-student`;
-            const {data} = await axios.get(url);
+  async GetDevInfo(accessToken) {
+    try {
+      const config = {
+        headers: {
+          Authorization: accessToken,
+        },
+      };
+      const url = `${SERVER_BLUE}/info/all-student`;
+      const { data } = await axios.get(url);
 
-            //const {data} = await axios.get(url, config);
-            return data;
-        } catch (error) {
-            console.log(error)
-        }
+      //const {data} = await axios.get(url, config);
+      return data;
+    } catch (error) {
+      console.log(error);
     }
+  }
 }
- 
+
 export default new MyProjectAPI();
--- src/assets/api/Project.js
+++ src/assets/api/Project.js
@@ -1,10 +1,10 @@
-import { SERVER } from "../../config/config.json";
+import { SERVER_RED, SERVER_BLUE } from "../../config/config.json";
 import axios from "axios";
 
 class Project {
   async GetProjects() {
     try {
-      const url = `${SERVER}/project`;
+      const url = `${SERVER_BLUE}/project`;
       const { data } = await axios.get(url);
 
       return data;
