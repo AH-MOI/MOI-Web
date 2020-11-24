4f2b39d39aafacc8ff13bd797c3ae2712eebb95a
CutyApple <dbtldhs0915@naver.com>
2020-11-24 19:44:57 +0900

    [FEAT] <LogModal.jsx> apply login

--- src/components/header/logModal/LogModal.jsx
+++ src/components/header/logModal/LogModal.jsx
@@ -1,11 +1,76 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
+import { AUTH } from 'lib/requestUrl';
+import { ACCESS_TOKEN, REFRESH_TOKEN, BASE_URL_LIST, methodType, requestApiWithBodyWithoutToken } from 'lib/requestApi';
 import "./LogModal.scss";
 
 const LogModal = (props) => {
+    const [log, setLog] = useState({
+        id: "",
+        password: "",
+        confirmPassword: "",
+        name: "",
+        birthday: "",
+        school: ""
+    })
+
+    const onChange = useCallback((name, e) => {
+        setLog({...log, [name]: e.target.value});
+    }, [log])
+
+
     const { modal } = props;
     const { setModal } = props;
 
     const [logMode, setLogMode] = useState(true);
+
+    const onSubmit = async(e) => {
+        e.preventDefault();
+
+        if(!!logMode) {
+            try {
+                const res = await requestApiWithBodyWithoutToken(BASE_URL_LIST.BLUE, methodType.POST, AUTH.login(), {
+                    id: log.id,
+                    password: log.password
+                });
+
+                window.localStorage.setItem(ACCESS_TOKEN, res.data[ACCESS_TOKEN]);
+                window.localStorage.setItem(REFRESH_TOKEN, res.data[REFRESH_TOKEN]);
+
+                alert("로그인에 성공했습니다.")
+                setModal(false);
+            } catch(err) {
+                switch (err.status) {
+                    case 404: {
+                        alert("아이디 또는 비밀번호가 틀렸습니다.");
+                        break;
+                    }
+                    default: {}
+                }
+            }
+        } else {
+            try {
+                const res = await requestApiWithBodyWithoutToken(BASE_URL_LIST.BLUE, methodType.POST, AUTH.register(), {
+                    id: log.id,
+                    password: log.password,
+                    confirmPassword: log.confirmPassword,
+                    name: log.name,
+                    birthday: log.birthday,
+                    school: log.school
+                });
+
+                alert("회원가입에 성공했습니다.")
+                setModal(false);
+            } catch(err) {
+                switch (err.status) {
+                    case 400: {
+                        alert("입력 정보가 잘못되었습니다.");
+                        break;
+                    }
+                    default: {}
+                }
+            }
+        }
+    }
 
     return (
         <div className="modal-wrap">
@@ -14,16 +79,15 @@ const LogModal = (props) => {
                 <div className="modal-header">
                     <p className="modal-header-text">{logMode ? "로그인" : "회원가입"}</p>
                 </div>
-                
                 {logMode ?    
-                <form className="modal-body">
+                <form className="modal-body" onSubmit={onSubmit}>
                     <div className="login-wrap">
                         <label htmlFor="login-input" className="login-label">아이디</label>
-                        <input type="text" className="login-input" placeholder="아이디"/>
+                        <input type="text" className="login-input" placeholder="아이디" onChange={(e) => onChange("id", e)} value={log.id}/>
                     </div>
                     <div className="login-wrap">
                         <label htmlFor="login-input" className="login-label">비밀번호</label>
-                        <input type="password" className="login-input" placeholder="비밀번호"/>
+                        <input type="password" className="login-input" placeholder="비밀번호" onChange={(e) => onChange("password", e)} value={log.password}/>
                     </div>
                     <div className="login-wrap btn-wrap">
                         <button className="login-btn" type="submit">로그인 하기</button>
@@ -33,41 +97,41 @@ const LogModal = (props) => {
                         <button className="register" onClick={() => {setLogMode(false)}}>회원가입</button>
                     </div>
                 </form> : 
-                <form className="modal-body">
+                <form className="modal-body" onSubmit={onSubmit}>
                     <div className="login-wrap">
                         <label htmlFor="login-input" className="login-label">아이디</label>
-                        <input type="text" className="login-input" placeholder="아이디"/>
+                        <input type="text" className="login-input" placeholder="아이디" onChange={(e) => onChange("id", e)} value={log.id}/>
                     </div>
                     <div className="login-wrap">
                         <label htmlFor="login-input" className="login-label">비밀번호</label>
-                        <input type="password" className="login-input" placeholder="비밀번호"/>
+                        <input type="password" className="login-input" placeholder="비밀번호" onChange={(e) => onChange("password", e)} value={log.password}/>
                     </div>
                     <div className="login-wrap">
                         <label htmlFor="login-input" className="login-label">비밀번호확인</label>
-                        <input type="password" className="login-input" placeholder="비밀번호확인"/>
+                        <input type="password" className="login-input" placeholder="비밀번호확인" onChange={(e) => onChange("confirmPassword", e)} value={log.confirmPassword}/>
                     </div>
                     <div className="login-wrap">
                         <label htmlFor="login-input" className="login-label">이름</label>
-                        <input type="text" className="login-input" placeholder="이름"/>
+                        <input type="text" className="login-input" placeholder="이름" onChange={(e) => onChange("name", e)} value={log.name}/>
                     </div>
                     <div className="login-wrap">
                         <label htmlFor="login-input" className="login-label">생년월일</label>
-                        <input type="date" className="login-input" />
+                        <input type="date" className="login-input" onChange={(e) => onChange("birthday", e)} value={log.birthday}/>
                     </div>
                     <div className="login-wrap">
                         <label htmlFor="login-input" className="login-label">학교</label>
                         <div className="school-radio-wrap">
                             <div className="login-radio-wrap">
                                 <span className="login-radio-span">광주</span>
-                                <input type="radio" className="login-input login-radio" name="school"/>
+                                <input type="radio" className="login-input login-radio" name="school" onClick={(e) => onChange("school", e)} value={"광주"}/>
                             </div>
                             <div className="login-radio-wrap">
                                 <span className="login-radio-span">대구</span>
-                                <input type="radio" className="login-input login-radio" name="school"/>
+                                <input type="radio" className="login-input login-radio" name="school" onClick={(e) => onChange("school", e)} value={"대구"}/>
                             </div>
                             <div className="login-radio-wrap">
                                 <span className="login-radio-span">대덕</span>
-                                <input type="radio" className="login-input login-radio" name="school"/>
+                                <input type="radio" className="login-input login-radio" name="school" onClick={(e) => onChange("school", e)} value={"대덕"}/>
                             </div>
                         </div>
                     </div>
