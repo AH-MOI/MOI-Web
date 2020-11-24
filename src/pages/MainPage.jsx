2a62f217d8933357393d227fdb0917d3099b9e9d
CutyApple <dbtldhs0915@naver.com>
2020-11-24 17:00:23 +0900

    [FEAT] <MainPage.jsx> Apply HeaderContainer

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import MainContainer from "containers/Main/MainContainer";
+import HeaderContainer from "containers/Header/HeaderContainer";
 
 const MainPage = () => {
   return (
     <>
+      <HeaderContainer />
       <MainContainer />
     </>
   );
