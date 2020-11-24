566a9b94cbc2d1161c76bd099045d9a66bd1e9ca
정성훈 <52942411+jsh0128@users.noreply.github.com>
2020-11-24 17:01:06 +0900

    [ADD] Write 컴포넌트 추가

--- src/stores/index.js
+++ src/stores/index.js
@@ -1,7 +1,9 @@
 import ProgressStore from "./Progress";
+import WriteStore from "./Write";
 
 const stores = {
   ProgressStore: new ProgressStore(),
+  WriteStore: new WriteStore(),
 };
 
 export default stores;
