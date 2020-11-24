e1c98ab252574c2b7bbd5b9da5e854f2556a20b1
정성훈 <52942411+jsh0128@users.noreply.github.com>
2020-11-25 08:11:39 +0900

    [UPDATE] last

--- src/containers/Write/WriteContainer.jsx
+++ src/containers/Write/WriteContainer.jsx
@@ -5,14 +5,19 @@ import stores from "stores";
 
 const WriteContainer = ({}) => {
   const { modal, showWriteModal, tryCreateProject } = stores.WriteStore;
-  const [date, setDate] = useState("");
   const [content, setContent] = useState("");
   const [title, setTitle] = useState("");
   const [tag, setTag] = useState("");
   const [hashTag, setHashTag] = useState([]);
-  // const [date, setDate] = useState(new Date());
+  const [date, setDate] = useState(new Date());
   const [selectedItem, setSelectedItem] = useState([]);
   const [show, setShow] = useState(false);
+  const [count, setCount] = useState(0);
+  const [arrayCount, setArrayCount] = useState([]);
+  const [personnel, setPersonnel] = useState("");
+  const [agoArrayCount, setAgoArrayCount] = useState([]);
+  const [all, setAll] = useState([]);
+  const [changeCount, setChangeCount] = useState();
 
   const keyPressListener = (e) => {
     if ((e.key === "Enter" || e.key === "NumpadEnter") && tag !== "") {
@@ -42,6 +47,41 @@ const WriteContainer = ({}) => {
     array.splice(index, 1);
     setSelectedItem(array);
   };
+
+  const stringPersonnel = () => {
+    for (let index = 0; index < arrayCount.length; index++) {
+      console.log(arrayCount[index]);
+      setPersonnel(
+        personnel + selectedItem[index] + "/" + arrayCount[index] + "|"
+      );
+    }
+  };
+  console.log(personnel);
+
+  const handleCreateTeam = () => {
+    tryCreateProject(title, content, date, personnel, tag)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const changeString = () => {
+    for (let index = 0; index < all.length; index++) {
+      if (all[index].idx === count) {
+        setPersonnel(
+          personnel + all[index].menu + "/" + all[index].number + "|"
+        );
+      }
+    }
+  };
+
+  useEffect(() => {
+    changeString();
+  }, [count, changeCount]);
+
   return (
     <>
       <Write
@@ -59,6 +99,12 @@ const WriteContainer = ({}) => {
         removeSelectedItem={removeSelectedItem}
         show={show}
         setShow={setShow}
+        count={count}
+        setCount={setCount}
+        all={all}
+        setAll={setAll}
+        setChangeCount={setChangeCount}
+        handleCreateTeam={handleCreateTeam}
       />
     </>
   );
